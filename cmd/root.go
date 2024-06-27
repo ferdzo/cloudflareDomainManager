7e3294fe1f8fd55b95d24943fd75f2541f7e030a
ferdzo <ferdzo@users.noreply.github.com>
2024-06-28 01:37:07 +0200

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2024 Andrej Ferdzo MickovHow to structure my whole package to be optimal? <andrej@ferdzo.xyz>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "",
+	Short: "Simple command-line app to configure domain DNS records",
+	Long: `A simple command-line app that can be used to create, remove,
+modify, show and everything in-between for DNS records on Cloudflare.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.secrets.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "cloudflareDomainManager/cmd"
+
+func main() {
+	cmd.Execute()
+}
