9e4d2a9a7b39862ebc14102c7a6754b644e3ea89
ferdzo <ferdzo@users.noreply.github.com>
2024-06-28 01:56:23 +0200

    remove .idea/

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "cloudflareDomainManager/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- secrets/Secret.go
+++
@@ -1,7 +0,0 @@
-package secrets
-
-type Secret struct {
-	X_Auth_Key   string
-	X_Auth_Email string
-	Zone_ID      string
-}
